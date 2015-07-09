8e16962fce111c29532e28c08aac459222ece71c
David James <d.james88@btinternet.com>
2015-07-09 12:07:07 +0100

    received data from api on ship model

--- app/controllers/ships_controller.rb
+++ app/controllers/ships_controller.rb
@@ -1,2 +1,10 @@
 class ShipsController < ApplicationController
+
+  def index
+    Ship.populate_database
+  end
+
+  # def self.populate_database
+
+  # end
 end
--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,9 +1,19 @@
+require 'multi_xml'
+require 'httparty'
+
 class Ship < ActiveRecord::Base
   has_many :locations
   has_many :destinations
-end
 
 
-def self.populate_database
+  def self.populate_database
+    response = HTTParty.get("http://www.fleetmon.com/api/p/personal-v1/myfleet/?username=#{ENV["FLEETMON_ID"]}&api_key=#{ENV["FLEETMON_API_KEY"]}&format=json")
+    count = 0
+    while count < response["objects"].length do 
+      
+    end
+    binding.pry
+    nil
+  end
 
 end

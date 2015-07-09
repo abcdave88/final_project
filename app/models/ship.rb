3160681fd4cf1a2cc8e6e32366f7c2c6453d1318
David James <d.james88@btinternet.com>
2015-07-09 12:44:59 +0100

    progress on ship data

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -8,12 +8,24 @@ class Ship < ActiveRecord::Base
 
   def self.populate_database
     response = HTTParty.get("http://www.fleetmon.com/api/p/personal-v1/myfleet/?username=#{ENV["FLEETMON_ID"]}&api_key=#{ENV["FLEETMON_API_KEY"]}&format=json")
-    count = 0
+
     while count < response["objects"].length do 
-      
+      count = 0
+      ship_name = response["objects"][count]["vessel"]["name"]
+      heading = response["objects"][count]["vessel"]["heading"]
+      status = response["objects"][count]["vessel"]["navigationstatus"]
+      last_port = response["objects"][count]["vessel"]["lastport"]["name"]
+      flag = response["objects"][count]["vessel"]["flag"]
+      photo = response["objects"][count]["vessel"]["photos"]
+      ship = Ship.new
+      ship.assign_attributes({:name "#{ship_name}", :heading "#{heading}", :status "#{status}", :lastport "#{last_port}", :flag "#{flag}", :photo "#{photo}"})
+      # @ship = Ship.new(:name "#{ship_name}", :heading "#{heading}", :status "#{status}", :lastport "#{last_port}", :flag "#{flag}", :photo "#{photo}")
+      ship.save
+      count =+1
+      binding.pry
+      nil
     end
-    binding.pry
-    nil
+
   end
 
 end

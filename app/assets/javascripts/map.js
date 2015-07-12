b9a963174b41eff3dd4f81d536379f35260d0b02
David James <d.james88@btinternet.com>
2015-07-12 16:27:46 +0100

    mapbox intialised

---
+++ app/assets/javascripts/map.js
@@ -0,0 +1,5 @@
+$(document).ready(function(){
+
+L.mapbox.accessToken = 'pk.eyJ1IjoiYWJjZGF2ZTg4IiwiYSI6IjU2MDE1YzUwYjI5NTg3YTExYTJiOWMwNmI2NWEzOWY4In0._qV9cnHz0Q0Uk28oJtUslQ';
+var map = L.mapbox.map('map', 'abcdave88.mmjnge0o').setView([40, -74.50], 9);
+})//end of document.ready

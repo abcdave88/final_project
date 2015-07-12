b40e0209c7f8b9afca38a6b234a38b5f694b30f5
David James <d.james88@btinternet.com>
2015-07-12 16:56:58 +0100

    map centered

--- app/assets/javascripts/map.js
+++ app/assets/javascripts/map.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 
 L.mapbox.accessToken = 'pk.eyJ1IjoiYWJjZGF2ZTg4IiwiYSI6IjU2MDE1YzUwYjI5NTg3YTExYTJiOWMwNmI2NWEzOWY4In0._qV9cnHz0Q0Uk28oJtUslQ';
-var map = L.mapbox.map('map', 'abcdave88.mmjnge0o').setView([40, -74.50], 9);
+var map = L.mapbox.map('map', 'abcdave88.mmjnge0o').setView([53.9560855309879, -2.8125], 3);
 })//end of document.ready
